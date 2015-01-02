00346ec21a7fbb1eaeaff09e229efb328824e30b
Atinderpal Singh <atindermann08@gmail.com>
2015-01-02 16:05:02 +0530

    specified port for hostname

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,7 +22,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
+			'host'      => '127.0.0.1:2083',
 			'database'  => 'hungerexpert',
 			'username'  => 'headmin',
 			'password'  => 'hungerpass',
