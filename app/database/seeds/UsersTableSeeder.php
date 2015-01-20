363dc0463af422260ba7806547a6a424dafb2e79
Atinderpal Singh <atindermann08@gmail.com>
2015-01-20 16:20:51 +0530

    Confide installed and configured

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -17,5 +17,72 @@ class UsersTableSeeder extends Seeder {
 			]);
 		}
 	}
+    public function run()
+    {
+        $user = new User;
+        $user->username = 'mann';
+        $user->email = 'atindermann08@gmail.com';
+        $user->password = 'mann';
+        $user->password_confirmation = 'mann';
+        $user->confirmation_code = md5(uniqid(mt_rand(), true));
+
+        if(! $user->save()) {
+            Log::info('Unable to create user '.$user->username, (array)$user->errors());
+        } else {
+            Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
+        }
+        
+        $user = new User;
+        $user->username = 'admin';
+        $user->email = 'admin@hungerexpert.in';
+        $user->password = 'hungerexpert';
+        $user->password_confirmation = 'hungerexpert';
+        $user->confirmation_code = md5(uniqid(mt_rand(), true));
+
+        if(! $user->save()) {
+            Log::info('Unable to create user '.$user->username, (array)$user->errors());
+        } else {
+            Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
+        }
+        
+        $user = new User;
+        $user->username = 'employee';
+        $user->email = 'employee@hungerexpert.in';
+        $user->password = 'employee';
+        $user->password_confirmation = 'employee';
+        $user->confirmation_code = md5(uniqid(mt_rand(), true));
+
+        if(! $user->save()) {
+            Log::info('Unable to create user '.$user->username, (array)$user->errors());
+        } else {
+            Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
+        }
+        
+        $user = new User;
+        $user->username = 'customer';
+        $user->email = 'customer@hungerexpert.in';
+        $user->password = 'customer';
+        $user->password_confirmation = 'customer';
+        $user->confirmation_code = md5(uniqid(mt_rand(), true));
+
+        if(! $user->save()) {
+            Log::info('Unable to create user '.$user->username, (array)$user->errors());
+        } else {
+            Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
+        }
+        
+        $user = new User;
+        $user->username = 'restaurant';
+        $user->email = 'restaurant@hungerexpert.in';
+        $user->password = 'restaurant';
+        $user->password_confirmation = 'restaurant';
+        $user->confirmation_code = md5(uniqid(mt_rand(), true));
+
+        if(! $user->save()) {
+            Log::info('Unable to create user '.$user->username, (array)$user->errors());
+        } else {
+            Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
+        }
+    }
 
 }
--- app/routes.php
+++ app/routes.php
@@ -64,3 +64,16 @@ Route::put('/order/:id_cart', 'updateOrder');
 if (file_exists(__DIR__.'/controllers/Server.php')) {
     Route::get('/deploy', 'Server@deploy');
 }
+//
+
+// Confide routes
+Route::get('users/create', 'UsersController@create');
+Route::post('users', 'UsersController@store');
+Route::get('users/login', 'UsersController@login');
+Route::post('users/login', 'UsersController@doLogin');
+Route::get('users/confirm/{code}', 'UsersController@confirm');
+Route::get('users/forgot_password', 'UsersController@forgotPassword');
+Route::post('users/forgot_password', 'UsersController@doForgotPassword');
+Route::get('users/reset_password/{token}', 'UsersController@resetPassword');
+Route::post('users/reset_password', 'UsersController@doResetPassword');
+Route::get('users/logout', 'UsersController@logout');
